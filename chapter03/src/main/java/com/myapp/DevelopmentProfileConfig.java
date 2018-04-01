b2a027dda3629075af79c26b7e0dc34a3e2d6c97
zhoumingjun <africancu@126.com>
2018-04-01 21:38:59 +0800

    添加第三章

---
+++ chapter03/src/main/java/com/myapp/DevelopmentProfileConfig.java
@@ -0,0 +1,22 @@
+package com.myapp;
+
+import javax.activation.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+@Configuration
+@Profile("dev")
+public class DevelopmentProfileConfig {
+
+	@Bean(destroyMethod = "shutdown")
+	public EmbeddedDatabase embeddedDataSource() {
+		return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).addScript("classpath:schema.sql")
+				.addScript("classpath:test-data.sql").build();
+	}
+
+}
